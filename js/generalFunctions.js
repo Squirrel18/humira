0aaea46f6b9acf26ebaefac7f78ead4e3ccacb51
Squirrel18 <sistemas4@creative-med.com>
2017-05-10 22:37:34 -0500

    Change slide-18

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -1,10 +1,11 @@
 function start() {
-
     resizeAnimation();
-    var elements = document.querySelectorAll("img");
-    for(let e of elements) {
-        e.ondragstart = function() { return false; };
-    }
+    var video = document.querySelectorAll("video");
+    /*for(var i = 0; i < video.length; i++) {
+        console.log(video[i]);
+        video[i].play();
+    }*/
+    //alert('video');
 }
 
 function videoCheck() {
@@ -39,30 +40,27 @@ window.onresize = function() {
 }
 
 function changeSlide(data) {
-    //alert(`slide-${data}.html`);
-    if(data === 0) {
-        window.location.assign("../index.html");
-        return;
-    }
-    window.location.assign(`slide-${data}.html`);
+    window.location.assign("slide-" + data + ".html");
 }
 
 function slide_5() {
     if(document.querySelectorAll(".slide-5-imgs")) {
-        let time = 1.0;
-        for(let e of Array.from(document.querySelectorAll(".slide-5-imgs"))) {
+        var time = 1;
+        var e = document.querySelectorAll(".slide-5-imgs");
+        for(var i = 0; i < e.length; i++) {
             time += 0.25;
-            e.style.WebkitAnimation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
-            e.style.animation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
+            e[i].style.WebkitAnimation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
+            e[i].style.animation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
         }
     }
 
     if(document.querySelectorAll(".slide-5-imgs-se")) {
-        let time = 3.25;
-        for(let e of Array.from(document.querySelectorAll(".slide-5-imgs-se"))) {
+        var time_1 = 3.25;
+        var e_1 = document.querySelectorAll(".slide-5-imgs-se");
+        for(var i = 0; i < e.length; i++) {
             time += 0.25;
-            e.style.WebkitAnimation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
-            e.style.animation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
+            e_1[i].style.WebkitAnimation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
+            e_1[i].style.animation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
         }
     }
 }
@@ -70,19 +68,23 @@ function slide_5() {
 //Function reel 
 function reelImage(){
 	
-	let idImage = document.getElementById("imgReel");
-	let size = idImage.getAttribute("data-reel-size");
-	let time = idImage.getAttribute("data-reel-time");
-	let rute = idImage.getAttribute("data-reel-src") + "/";
-	let type = "." + idImage.getAttribute("data-type");
-	let timeStart = idImage.getAttribute("data-start");
-	let loop = idImage.getAttribute("data-loop");
-	let cont = 0;
+	var idImage = document.getElementById("imgReel");
+	var size = idImage.getAttribute("data-reel-size");
+	var time = idImage.getAttribute("data-reel-time");
+	var rute = idImage.getAttribute("data-reel-src") + "/";
+	var type = "." + idImage.getAttribute("data-type");
+	var timeStart = idImage.getAttribute("data-start");
+	var loop = idImage.getAttribute("data-loop");
+	var cont = 0;
 		
-	let validate = loop ? true : false;
-	console.log(validate);
+    if(loop) {
+        validate = true;
+    } else {
+        validate = false;
+    }
+
 	setTimeout(function() {
-	    let interval = setInterval(function() {
+	    var interval = setInterval(function() {
 		    idImage.src = rute + cont + type;
 			cont++;
             if(cont > size) {
@@ -99,32 +101,34 @@ function reelImage(){
 	
 function slide_6() {
     if(document.querySelectorAll(".slide-6-imgs")) {
-        let time = 0.25;
-        for(let e of Array.from(document.querySelectorAll(".slide-6-imgs"))) {
+        var time = 0.25;
+        var e = document.querySelectorAll(".slide-6-imgs");
+        for(var i = 0; i < e.length; i++) {
             time += 0.25;
-            e.style.WebkitAnimation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
-            e.style.animation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
+            e[i].style.WebkitAnimation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
+            e[i].style.animation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
         }
     }
 }
 
 function slide_6_1() {
     if(document.querySelectorAll(".slide-6-imgs")) {
-        let time = 0.25;
-        for(let e of Array.from(document.querySelectorAll(".slide-6-imgs"))) {
-            e.style.WebkitAnimation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
-            e.style.animation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
+        var time = 0.25;
+        var e = document.querySelectorAll(".slide-6-imgs");
+        for(var i = 0; i < e.length; i++) {
+            e[i].style.WebkitAnimation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
+            e[i].style.animation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
         }
     }
 }
 
-let checkArray = [true,true,true,true,true,true,true,true,true];
+var checkArray = [true,true,true,true,true,true,true,true,true];
 
 function slide_6_function(data) {
 
-    let contain = document.querySelector(".slide-6-contain");
-    let element = contain.children[data];
-    let tempImg = document.querySelector(`.slide-6-${data}`);
+    var contain = document.querySelector(".slide-6-contain");
+    var element = contain.children[data];
+    var tempImg = document.querySelector(".slide-6-" + data);
 
     if(checkArray[data]) {
         element.style.WebkitAnimation = `rotateYwise 750ms ease-in-out forwards`;
@@ -132,7 +136,7 @@ function slide_6_function(data) {
         setTimeout(function() {
             element.style.backgroundRepeat = "no-repeat";
             element.style.backgroundSize = "contain";
-            element.style.backgroundImage = `url('../assets/slide-6-${data}-${data}.png')`;
+            element.style.backgroundImage = "url('../assets/slide-6-" + data + "-" + data + ".png')";
         }, 500);
         tempImg.style.display = "none";
         checkArray[data] = false;
@@ -150,11 +154,12 @@ function slide_6_function(data) {
 
 function slide_16() {
     if(document.querySelector(".slide-16-1-contain")) {
-        let time = 0.25;
-        for(let e of Array.from(document.querySelector(".slide-16-1-contain").children)) {
+        var time = 0.25;
+        var e = document.querySelector(".slide-16-1-contain").children;
+        for(var i = 0; i < e.length; i++) {
             time += 0.25;
-            e.style.WebkitAnimation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
-            e.style.animation = `fadeOpen 750ms ease-in-out forwards ${time}s`;
+            e[i].style.WebkitAnimation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
+            e[i].style.animation = 'fadeOpen 750ms ease-in-out forwards ' + time + 's';
         }
     }
 }
